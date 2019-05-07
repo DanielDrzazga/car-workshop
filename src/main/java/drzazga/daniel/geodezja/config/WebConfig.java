28e42b541a4ffbbb002ce80946d176f1adc7e569
Daniel Drzazga <whater.d2@gmail.com>
2019-05-07 19:17:56 +0200

    creating a webconfig and adding the orc mapperFacade

---
+++ src/main/java/drzazga/daniel/geodezja/config/WebConfig.java
@@ -0,0 +1,17 @@
+package drzazga.daniel.geodezja.config;
+
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public MapperFacade mapper(){
+        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+        return mapperFactory.getMapperFacade();
+    }
+}
