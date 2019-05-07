80736f357b91c8e56748d53ac39aea16e1597923
Daniel Drzazga <whater.d2@gmail.com>
2019-05-07 14:41:35 +0200

    creation userDto

---
+++ src/main/java/drzazga/daniel/geodezja/Dtos/UserDto.java
@@ -0,0 +1,72 @@
+package drzazga.daniel.geodezja.Dtos;
+
+import java.util.Set;
+
+public class UserDto {
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Integer active;
+    private Set<RoleDto> rolesDto;
+
+    public UserDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+
+    public Set<RoleDto> getRolesDto() {
+        return rolesDto;
+    }
+
+    public void setRolesDto(Set<RoleDto> rolesDto) {
+        this.rolesDto = rolesDto;
+    }
+}
