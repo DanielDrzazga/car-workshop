c59ecf3568c59f48e7826a1503263e7842e4011f
Daniel Drzazga <whater.d2@gmail.com>
2019-05-07 16:24:45 +0200

    creating user registration Dto

---
+++ src/main/java/drzazga/daniel/geodezja/Dtos/UserRegistrationDto.java
@@ -0,0 +1,60 @@
+package drzazga.daniel.geodezja.Dtos;
+
+import drzazga.daniel.geodezja.annotations.CorrectUserEmail;
+import drzazga.daniel.geodezja.annotations.CorrectUserPassword;
+import drzazga.daniel.geodezja.annotations.UniqueUserEmail;
+
+import javax.validation.constraints.NotBlank;
+
+public class UserRegistrationDto {
+
+    @UniqueUserEmail
+    @CorrectUserEmail
+    @NotBlank(message = "error.userEmail.empty")
+    private String email;
+
+    @CorrectUserPassword
+    @NotBlank(message = "error.userPassword.empty")
+    private String password;
+
+    @NotBlank(message = "error.userName.empty")
+    private String firstName;
+
+    @NotBlank(message = "error.userLastName.empty")
+    private String lastName;
+
+    public UserRegistrationDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
