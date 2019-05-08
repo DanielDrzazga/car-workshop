64fedbdaa1f1520d6466a1678f3ca9657aa18122
Daniel Drzazga <whater.d2@gmail.com>
2019-05-08 09:18:21 +0200

    UserService was updated, added - findByEmail - saveUser

--- src/main/java/drzazga/daniel/geodezja/services/UserService.java
+++ src/main/java/drzazga/daniel/geodezja/services/UserService.java
@@ -1,5 +1,14 @@
 package drzazga.daniel.geodezja.services;
 
+import drzazga.daniel.geodezja.Dtos.UserDto;
+import drzazga.daniel.geodezja.Dtos.UserRegistrationDto;
+
 public interface UserService {
-    boolean isEmailExist(String value);
+
+    boolean isEmailExist(String email);
+
+    UserDto findByEmail(String email);
+
+    void saveUser(UserRegistrationDto userRegistrationDto);
+
 }
--- src/main/java/drzazga/daniel/geodezja/services/impl/UserServiceImpl.java
+++ src/main/java/drzazga/daniel/geodezja/services/impl/UserServiceImpl.java
@@ -1,22 +1,57 @@
 package drzazga.daniel.geodezja.services.impl;
 
+import drzazga.daniel.geodezja.Dtos.UserDto;
+import drzazga.daniel.geodezja.Dtos.UserRegistrationDto;
+import drzazga.daniel.geodezja.model.Role;
+import drzazga.daniel.geodezja.model.User;
+import drzazga.daniel.geodezja.repositories.RoleRepository;
 import drzazga.daniel.geodezja.repositories.UserRepository;
 import drzazga.daniel.geodezja.services.UserService;
+import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final MapperFacade mapperFacade;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, MapperFacade mapperFacade) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.mapperFacade = mapperFacade;
     }
 
     @Override
     public boolean isEmailExist(String email) {
         return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public UserDto findByEmail(String email) {
+        return mapperFacade.map(userRepository.findByEmail(email), UserDto.class);
+    }
+
+    @Override
+    public void saveUser(UserRegistrationDto userRegistrationDto) {
+        User user = mapperFacade.map(userRegistrationDto, User.class);
+        user.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+        user.setActive(1);
+
+        Role role = roleRepository.findByRole("ROLE_ADMIN");
+        user.setRoles(new HashSet<>(Arrays.asList(role)));
+
+        userRepository.save(user);
     }
 }
